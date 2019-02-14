efd49b85192322ca2c0339e2dcae708e0b4f48cf
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-13 17:15:41 -0700

    Adds routes for / and /new-note

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter, Route } from 'react-router-dom';
+import NoteContainer from '../NoteContainer/NoteContainer';
+import NoteForm from '../NoteForm/NoteForm';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Trapper Keeper!!</h1>
+        <Route path='/new-note' component={NoteForm}/>
+        <Route path='/' component={NoteContainer}/>
       </div>
     );
   }
