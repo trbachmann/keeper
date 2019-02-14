37a0862ef052c65906507d8f19e199b00e0b4a7c
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-13 17:18:47 -0700

    Connect NoteContainer to redux store

---
+++ src/containers/NoteContainer/NoteContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './NoteContainer.scss';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { NoteCard } from '../NoteCard/NoteCard';
+import { Link } from 'react-router-dom';
+
+export const NoteContainer = ({ notes }) => {
+  const cards = notes.map(note => {
+    return <NoteCard {...note}/>
+  });
+
+  return (
+    <div className='NoteContainer'>
+      <Link to='/new-note'>New Note</Link>
+      {cards}
+    </div>
+  );
+};
+
+export const mapStateToProps = (state) => ({
+  notes: state.notes
+});
+
+export default connect(mapStateToProps)(NoteContainer);
+
+NoteContainer.propTypes = {
+  notes: PropTypes.array
+}
