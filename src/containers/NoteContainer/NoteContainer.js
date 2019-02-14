0b9ab06c3ab164958edbef1f0d8438901a2fe37e
Whitney Burton <whitneyburton9@gmail.com>
2019-02-14 14:44:21 -0700

    Add Masonry component to NoteContainer

--- src/containers/NoteContainer/NoteContainer.js
+++ src/containers/NoteContainer/NoteContainer.js
@@ -4,18 +4,22 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { NoteCard } from '../NoteCard/NoteCard';
 import { Link } from 'react-router-dom';
+import Masonry from 'react-masonry-css';
 
 export const NoteContainer = ({ notes }) => {
   const cards = notes.map(note => {
     return <NoteCard {...note} />
-  });
+  }).reverse();
 
   return (
     <div className='NoteContainer'>
       <Link to='/new-note' className='NoteContainer--new-note'>New Note</Link>
-      <div className='NoteContainer--cards'>
+      <Masonry
+        breakpointCols={4}
+        className='NoteContainer--cards'
+        columnClassName='NoteContainer--cards-masonry-cols'>
         {cards}
-      </div>
+      </Masonry>
     </div>
   );
 };
