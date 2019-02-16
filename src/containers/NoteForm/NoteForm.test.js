0ecfb5f1f3023de0fb8f11fd2cb36f2e8a4c324e
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-15 21:36:16 -0700

    Test default state and snapshots

--- src/containers/NoteForm/NoteForm.test.js
+++ src/containers/NoteForm/NoteForm.test.js
@@ -1,7 +1,38 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import React from 'react';
+import { shallow } from 'enzyme';
+import { NoteForm } from './NoteForm';
+import { mockNote } from '../../mockNotes';
+
+jest.mock('shortid', () => ({ generate: jest.fn(() => '') }));
 
 describe('NoteForm', () => {
-  let wrapper
+  let wrapper;
+  let wrapperNewNote;
+  const { id, title, listItems } = mockNote;
+  const mockMatch = { params: { id }, path: `/notes/${id}` }
+  const mockMatchNewNote = { params: {}, path: '/new-note' }
+  const mockProps = { match: mockMatch, title, listItems }
+  const mockPropsNewNote = {...mockProps, match: mockMatchNewNote}
 
-})
+  beforeEach(() => {
+    wrapper = shallow(<NoteForm {...mockProps}/>);
+    wrapperNewNote = shallow(<NoteForm {...mockPropsNewNote}/>);
+  });
+
+  it('should have default state', () => {
+    expect(wrapperNewNote.state()).toEqual({
+      title: '',
+      listItems: [],
+      status: 0,
+      focusedListItemID: null
+    });
+  });
+
+  it('should match the snapshot when the path is /new-note', () => {
+    expect(wrapperNewNote).toMatchSnapshot();
+  });
+
+  it('should match the snapshot when the path is /notes/:id', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
