b97e4f46465dd82ac38a9d2e0d609bb108e5814f
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-15 17:34:48 -0700

    Add mock data to use for notesReducer testing

--- src/mockNotes/index.js
+++ src/mockNotes/index.js
@@ -58,7 +58,7 @@ export const mockNotes = [
       }
     ]
   }
-]
+];
 
 export const mockNote = {
   id: 'rzz',
@@ -75,4 +75,119 @@ export const mockNote = {
       isComplete: false
     }
   ]
-}
+};
+
+export const mockNotesAfterDelete = [
+  { 
+    id: 'ijf', 
+    title: 'Learn Redux', 
+    listItems: [
+      { 
+        id: 'asg', 
+        description: 'read through lesson', 
+        isComplete: false
+      }, 
+      { 
+        id: 'rhf', 
+        description: 'follow along with tutorial', 
+        isComplete: false
+      }
+    ]
+  },
+  { 
+    id: 'pdo', 
+    title: 'Start portfolio', 
+    listItems: [
+      { 
+        id: 'cat', 
+        description: 'Setup user account', 
+        isComplete: false
+      }
+    ]
+  },
+  { 
+    id: 'fah', 
+    title: 'Make dinner', 
+    listItems: [
+      { 
+        id: 'ley', 
+        description: 'gather ingredients', 
+        isComplete: false
+      }, 
+      { 
+        id: 'mei', 
+        description: 'preheat oven', 
+        isComplete: false
+      }
+    ]
+  }
+];
+
+export const mockUpdatedNote = {
+    id: 'ijf', 
+    title: 'Learn Redux', 
+    listItems: [
+      { 
+        id: 'asg', 
+        description: 'read through lesson', 
+        isComplete: true
+      }, 
+      { 
+        id: 'rhf', 
+        description: 'follow along with tutorial', 
+        isComplete: false
+      },
+      {
+        id: 'ypu',
+        description: 'use redux in a project',
+        isComplete: false
+      }
+    ]
+};
+
+export const mockNotesAfterUpdating = [
+  mockUpdatedNote,
+  { 
+    id: 'pdo', 
+    title: 'Start portfolio', 
+    listItems: [
+      { 
+        id: 'cat', 
+        description: 'Setup user account', 
+        isComplete: false
+      }
+    ]
+  },
+  { 
+    id: 'bah', 
+    title: 'Grocery shopping', 
+    listItems: [
+      { 
+        id: 'rey', 
+        description: 'Stop by King Soopers', 
+        isComplete: false
+      }, 
+      { 
+        id: 'moo', 
+        description: 'Stop by Sprouts', 
+        isComplete: false
+      }
+    ]
+  },
+  { 
+    id: 'fah', 
+    title: 'Make dinner', 
+    listItems: [
+      { 
+        id: 'ley', 
+        description: 'gather ingredients', 
+        isComplete: false
+      }, 
+      { 
+        id: 'mei', 
+        description: 'preheat oven', 
+        isComplete: false
+      }
+    ]
+  }
+];
