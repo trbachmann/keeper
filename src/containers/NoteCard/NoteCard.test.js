0e5be34c759eb720d49f42e28210c429e49be0c2
Whitney Burton <whitneyburton9@gmail.com>
2019-02-16 18:32:25 -0700

    Add NoteCard snapshot test

--- src/containers/NoteCard/NoteCard.test.js
+++ src/containers/NoteCard/NoteCard.test.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import React from 'react';
+import { shallow } from 'enzyme';
+import NoteCard from './NoteCard';
+import { mockNote } from '../../mockNotes';
 
 describe('NoteCard', () => {
-  let wrapper
+  let wrapper;
 
-})
+  beforeEach(() => {
+    wrapper = shallow(
+      <NoteCard {...mockNote} />
+    );
+  });
+
+  it('should pass snapshot test', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
