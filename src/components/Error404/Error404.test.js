0d0afde8d7c99d01bce2561189f1c22e872ea23e
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-16 18:17:37 -0700

    Add snapshot and test for Error404 Resolves #37

--- src/components/Error404/Error404.test.js
+++ src/components/Error404/Error404.test.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import React from 'react';
+import { shallow } from 'enzyme';
+import Error404 from './Error404';
 
 describe('Error404', () => {
-  let wrapper
-
-})
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<Error404 />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
