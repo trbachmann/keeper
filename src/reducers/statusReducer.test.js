75a34237ad3ec15ba3c16a3bebd20bc706524090
Whitney Burton <whitneyburton9@gmail.com>
2019-02-16 18:03:36 -0700

    Add statusReducer tests

--- src/reducers/statusReducer.test.js
+++ src/reducers/statusReducer.test.js
@@ -1,1 +1,15 @@
 import { statusReducer } from './statusReducer';
+import { setStatus } from '../actions';
+
+describe('statusReducer', () => {
+  it('should return the default state of 0', () => {
+    const result = statusReducer(undefined, {});
+    expect(result).toEqual(0);
+  });
+
+  it('should return a code when called with SET_STATUS action', () => {
+    const initialState = 0;
+    const result = statusReducer(initialState, setStatus(201));
+    expect(result).toEqual(201);
+  });
+});
