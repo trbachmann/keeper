d7f7595533231b68208867439e0b5976b9ba748d
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-19 14:34:24 -0700

    Test handleDrop and handleDrag

--- src/containers/NoteCard/NoteCard.test.js
+++ src/containers/NoteCard/NoteCard.test.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import NoteCard from './NoteCard';
-import { mockNote, mockNoteLong } from '../../mockNotes';
+import { NoteCard } from './NoteCard';
+import { mockNotes, mockNote, mockNoteLong } from '../../mockNotes';
 
 describe('NoteCard', () => {
   let wrapper;
+  const mockProps = {
+    ...mockNote,
+    index: 0,
+    notes: mockNotes,
+    putAllNotes: jest.fn()
+  }
+  const mockEvent = {
+    dataTransfer: {
+      setData: jest.fn(),
+      getData: jest.fn(() => 1)
+    },
+    preventDefault: jest.fn()
+  };
 
   beforeEach(() => {
-    wrapper = shallow(<NoteCard {...mockNote} />);
+    wrapper = shallow(<NoteCard {...mockProps} />);
   });
 
   it('should pass snapshot test', () => {
@@ -17,5 +30,27 @@ describe('NoteCard', () => {
   it('should pass snapshot test when there are more than 10 items', () => {
     wrapper = shallow(<NoteCard {...mockNoteLong} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleDrag', () => {
+    it('should call event.dataTransfer.setData with the correct params', () => {
+      wrapper.find('.NoteCard--background-lavender').simulate('dragstart', mockEvent);
+      expect(mockEvent.dataTransfer.setData).toHaveBeenCalledWith('draggedIndex', 0);
+    });
+  });
+  
+  describe('handleDragOver', () => {
+    it('should call event.preventDefault()', () => {
+      wrapper.find('.NoteCard--background-lavender').simulate('dragover', mockEvent);
+      expect(mockEvent.preventDefault).toHaveBeenCalled();
+    });
+  });
+
+  describe('handleDrop', () => {
+    it('should call putAllNotes with the updated array of notes', () => {
+      wrapper.find('.NoteCard--background-lavender').simulate('drop', mockEvent);
+      const expected = [mockNotes[1], mockNotes[0], ...mockNotes.slice(2)];
+      expect(mockProps.putAllNotes).toHaveBeenCalledWith(expected);
+    });
   });
 });
