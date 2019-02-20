fbb7f753e62004a493fe3d170d3e336b550fa3eb
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-19 17:40:35 -0700

    Adds tests for queryReducer

---
+++ src/reducers/queryReducer.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { queryReducer } from './queryReducer';
+import { setQuery } from '../actions';
+
+describe('queryReducer', () => {
+  it('should return the default state', () => {
+    const expected = '';
+    const result = queryReducer(undefined, {});
+    expect(result).toEqual(expected);
+  });
+
+  it('should return state with the new query', () => {
+    const query = 'doge';
+    const initialState = '';
+    const result = queryReducer(initialState, setQuery(query));
+    expect(result).toEqual(query);
+  });
+});
