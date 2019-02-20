3813a5e0db45a5811ac4662cad0664f47d9638a7
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-19 17:10:45 -0700

    Add functionality to toggle list items and delete note from NoteCard

--- src/containers/NoteCard/NoteCard.js
+++ src/containers/NoteCard/NoteCard.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { putNote } from '../../thunks/putNote';
 import { putAllNotes } from '../../thunks/putAllNotes';
+import { deleteNoteThunk } from '../../thunks/deleteNoteThunk';
 import uncheckedicon from '../../images/uncheckedicon.svg';
 import checkedicon from '../../images/checkedicon.svg';
 import editicon from '../../images/edit.svg';
 import deleteicon from '../../images/delete.svg';
+import { setStatus } from '../../actions';
 
 export class NoteCard extends Component {
+  handleComplete = async (itemID) => {
+    const { id, title, color, listItems, putNote, setStatus } = this.props;
+    const updatedListItems = listItems.map(item => {
+      const { isComplete } = item;
+      return item.id === itemID ? { ...item, isComplete: !isComplete } : item;
+    });
+    await putNote({ id, title, color, listItems: updatedListItems });
+    setStatus(0);
+  }
+
+  handleDelete = async () => {
+    const { id, deleteNoteThunk, setStatus } = this.props;
+    await deleteNoteThunk(id);
+    setStatus(0);
+  }
+
   handleDrag = (event) => {
     event.dataTransfer.setData('draggedIndex', this.props.index);
   }
@@ -35,6 +54,7 @@ export class NoteCard extends Component {
               src={uncheckedicon}
               className='NoteForm--icon--unchecked'
               alt='unchecked icon'
+              onClick={() => this.handleComplete(id)}
             />
             <p className='NoteForm--p--incomplete'>{description}</p>
           </span>
@@ -48,6 +68,7 @@ export class NoteCard extends Component {
               src={checkedicon}
               className='NoteForm--icon--checked'
               alt='checked icon'
+              onClick={() => this.handleComplete(id)}
             />
             <p className='NoteForm--p--complete'>{description}</p>
           </span>
@@ -59,21 +80,26 @@ export class NoteCard extends Component {
       result.push(<p className='NoteCard--ellipsis' key='...'>...</p>);
     }
     return (
-      <Link to={'/notes/' + id}>
-        <div 
-          draggable
-          onDragStart={this.handleDrag}
-          onDragOver={(event) => event.preventDefault()}
-          onDrop={this.handleDrop}
-          className={'NoteCard--background-' + color}>
-          <h3 className='NoteCard--h3'>{title}</h3>
-          <div>{result}</div>
-          <span className='NoteCard--icon-container'>
-            <img className='NoteCard--delete-icon' src={editicon} alt='edit icon'/>
-            <img className='NoteCard--edit-icon' src={deleteicon} alt='delete icon'/>
-          </span>
-        </div>
-      </Link>
+      <div 
+        draggable
+        onDragStart={this.handleDrag}
+        onDragOver={(event) => event.preventDefault()}
+        onDrop={this.handleDrop}
+        className={'NoteCard--background-' + color}>
+        <h3 className='NoteCard--h3'>{title}</h3>
+        <div>{result}</div>
+        <span className='NoteCard--icon-container'>
+          <Link to={'/notes/' + id}>
+            <img className='NoteCard--edit-icon' src={editicon} alt='edit icon'/>
+          </Link>
+          <img
+            className='NoteCard--delete-icon'
+            src={deleteicon}
+            alt='delete icon'
+            onClick={this.handleDelete}
+          />
+        </span>
+      </div>
     );
   }
 }
@@ -83,7 +109,10 @@ export const mapStateToProps = (state) => ({
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-  putAllNotes: (notes) => dispatch(putAllNotes(notes))
+  putAllNotes: (notes) => dispatch(putAllNotes(notes)),
+  putNote: (note) => dispatch(putNote(note)),
+  deleteNoteThunk: (id) => dispatch(deleteNoteThunk(id)),
+  setStatus: (code) => dispatch(setStatus(code))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NoteCard);
@@ -94,5 +123,8 @@ NoteCard.propTypes = {
   listItems: PropTypes.array,
   color: PropTypes.string,
   notes: PropTypes.array,
-  putAllNotes: PropTypes.func
+  putAllNotes: PropTypes.func,
+  putNote: PropTypes.func,
+  deleteNote: PropTypes.func,
+  setStatus: PropTypes.func
 }
