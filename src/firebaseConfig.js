421d35e4b5e5e61052dcffa89abeada5992bcbc7
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-05 13:06:05 -0800

    Configure firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7Pu_Sta0xfZgdOEGyKUI5pbU2gBn1zI0",
+  authDomain: "keeper-233521.firebaseapp.com",
+  databaseURL: "https://keeper-233521.firebaseio.com",
+  projectId: "keeper-233521",
+  storageBucket: "keeper-233521.appspot.com",
+  messagingSenderId: "906176288821"
+};
