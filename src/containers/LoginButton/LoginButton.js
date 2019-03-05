81853c2e54fc785d4622aca40dd2130aa5a3b830
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-05 15:10:25 -0800

    Add functionality for setting user in redux store

--- src/containers/LoginButton/LoginButton.js
+++ src/containers/LoginButton/LoginButton.js
@@ -3,13 +3,23 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import googleicon from '../../images/googleicon.svg';
 import PropTypes from 'prop-types';
+import { fetchData, createOptions } from '../../utils/api';
+import { setUser, setNotes } from '../../actions';
+import { connect } from 'react-redux';
 
 export class LoginButton extends Component {
   handleClick = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     try{
-      const { user } = await firebase.auth().signInWithPopup(provider);
-      console.log('user', user);
+      const googleData = await firebase.auth().signInWithPopup(provider);
+      const { displayName, email, uid } = googleData.user;
+      const user = { displayName, email, uid };
+      const url = 'http://localhost:3001/api/v1/users';
+      const options = createOptions('POST', user);
+      const response = await fetchData(url, options);
+      const { notes } = await response.json();
+      this.props.setUser(user);
+      this.props.setNotes(notes);
     } catch (error) {
       console.log('error', error);
     }
@@ -27,6 +37,14 @@ export class LoginButton extends Component {
   }
 }
 
+export const mapDispatchToProps = (dispatch) => ({
+  setUser: (user) => dispatch(setUser(user)),
+  setNotes: (notes) => dispatch(setNotes(notes))
+});
+
+export default connect(null, mapDispatchToProps)(LoginButton);
+
 LoginButton.propTypes = {
-  
+  setUser: PropTypes.func,
+  setNotes: PropTypes.func
 }
