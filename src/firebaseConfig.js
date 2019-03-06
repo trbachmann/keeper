6887f30bc92a2647a7e8706765018acf52686015
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-06 13:52:52 -0800

    Update firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,4 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyB7Pu_Sta0xfZgdOEGyKUI5pbU2gBn1zI0",
-  authDomain: "keeper-233521.firebaseapp.com",
-  databaseURL: "https://keeper-233521.firebaseio.com",
-  projectId: "keeper-233521",
-  storageBucket: "keeper-233521.appspot.com",
-  messagingSenderId: "906176288821"
+  authDomain: "keeper-233521.firebaseapp.com"
 };
